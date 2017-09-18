12ac1f9daa843fbcd8f5010b8550ceb5ab3c14e6
Dmitri Kunin <whitemarten@gmail.com>
2017-09-18 23:24:04 +0300

    In progress of adding func tests

--- tests/basic-check.js
+++ tests/basic-check.js
@@ -1,12 +1,36 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        // slowMo: true,
+        // headless: false
+    });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({ path: 'example.png' });
+    await page.emulate(devices['iPhone 6']);
+    await page.goto('http://localhost:3000');
+
+    await page.screenshot({ path: './tests/screens/01-main-screen.png' });
+
+    await page.click('.md-toolbar button');
+    await page.waitFor(500);
+    await page.screenshot({ path: './tests/screens/02-side-menu.png' });
+    await page.click('div.md-sidenav-content > ul > li:nth-child(1) > button');
+    await page.waitFor(500);
+    await page.screenshot({ path: './tests/screens/03-add-habit-clicked.png' });
+
+    await page.focus('ul > li:nth-child(1) > div > div > input');
+    await page.type('Drink more whater');
+    // await page.click('.md-checkbox-container input');
+    await page.waitFor(500);
+    // await page.focus('body > div > div.page > ul > li:nth-child(5) > div > div > input');
+    // await page.type(333);
+    await page.screenshot({ path: './tests/screens/04-new-habit-input.png' });
+    await page.waitFor(500);
+    await page.click('ul > li:nth-child(5) > div > button');
+    await page.screenshot({ path: './tests/screens/05-finished-creating-new-habit.png' });
 
     await browser.close();
 })();
---
+++ tests/main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { spawn, exec } = require('child_process');
+const hrstart = process.hrtime();
+const testFiles = ['./tests/basic-check.js'];
+const express = require('express');
+const app = express();
+
+// exec('npm run build', () => {
+    let hrend = process.hrtime(hrstart);
+    console.info('Build time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+
+    Promise.all(testFiles.map(runSingleTest))
+        .then(() => {
+            let hrend = process.hrtime(hrstart);
+            console.info('Tests finished (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
+            process.exit(1);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+// });
+
+app.use(express.static('./dist'));
+app.listen(3000, () => {});
+
+function runSingleTest(file) {
+    return new Promise(resolve => {
+        const child = spawn('node', [file]);
+        child.on('close', resolve);
+    });
+}
